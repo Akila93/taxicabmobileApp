03d2e238daea969d9d930e6254784e2633abd68c
Akila93 <akila.13@cse.mrt.ac.lk>
2015-12-16 03:20:44 -0800

    ActivityMainfest edited and thread added to lunch app

--- app/src/main/java/com/app/service/taxi/taxiserviceapp/MainActivity.java
+++ app/src/main/java/com/app/service/taxi/taxiserviceapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.app.service.taxi.taxiserviceapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -28,9 +29,17 @@ public class MainActivity extends AppCompatActivity {
         });
         Thread send_activity_thr = new Thread(){
             public void run(){
-
+                try{
+                    sleep(3000);
+                }catch(Exception e){
+                
+                }finally {
+                    Intent startIntent = new Intent("com.app.taxi.service.SEND_ACTIVITY");
+                    startActivity(startIntent);
+                }
             }
         };
+        send_activity_thr.start();
     }
 
     @Override
